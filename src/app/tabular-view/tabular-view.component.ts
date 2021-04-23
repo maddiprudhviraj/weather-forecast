5ece919948fe143f8ab0a47243e833644b5f6ccb
maddiprudhviraj <maddiprudhviraj@gmail.com>
2021-04-23 10:53:03 +0530

    add ag-grid

---
+++ src/app/tabular-view/tabular-view.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabular-view',
+  templateUrl: './tabular-view.component.html',
+  styleUrls: ['./tabular-view.component.css']
+})
+export class TabularViewComponent implements OnInit {
+
+  columnDefs = [{ field: "make" }, { field: "model" }, { field: "price" }];
+
+  rowData = [
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxter", price: 72000 }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
